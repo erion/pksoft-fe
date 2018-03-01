eb6f90e7243166fc2f8993cd5e5dd828468181b4
erion <erion.dreyer@gmail.com>
2018-03-01 04:45:11 -0300

    adding user box

--- src/material-components/menu.js
+++ src/material-components/menu.js
@@ -1,21 +1,28 @@
 import React from 'react';
 
+import UserBox from './user.js';
+
 import Paper from 'material-ui/Paper';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 
-const style = {
+import PatientIcon from 'material-ui/svg-icons/maps/directions-walk';
+import TreatmentIcon from 'material-ui/svg-icons/action/assignment';
+import HistoryIcon from 'material-ui/svg-icons/action/history';
+
+const paperStyle = {
   display: 'inline-block',
   margin: '0 32px 16px 0'
 };
 
 const AppMenu = () => (
   <div>
-    <Paper style={style}>
+    <Paper style={paperStyle}>
       <Menu>
-        <MenuItem primaryText="Pacientes" />
-        <MenuItem primaryText="Tratamentos" />
-        <MenuItem primaryText="Histórico" />
+        <UserBox />
+        <MenuItem primaryText="Pacientes" leftIcon={<PatientIcon />} />
+        <MenuItem primaryText="Tratamentos" leftIcon={<TreatmentIcon />} />
+        <MenuItem primaryText="Histórico" leftIcon={<HistoryIcon />} />
       </Menu>
     </Paper>
   </div>
