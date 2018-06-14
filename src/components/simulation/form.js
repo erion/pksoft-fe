77951c74dd75a82d759338ed11bd6b8b11dbf85a
erion <erion.dreyer@gmail.com>
2018-06-14 02:00:48 -0300

    removing simulation form validation as a quick fix for back in history generate form error with all field having a value

--- src/components/simulation/form.js
+++ src/components/simulation/form.js
@@ -12,23 +12,12 @@ export default class SimulationForm extends React.Component {
     let simulation = SimulationModel
     simulation['cod_paciente'] = props.location.state.patientId
 
-    let formError, errorMessage = {}
-    formError = {formError: true}
-    for(let key in simulation) {
-      errorMessage[key] = {value: null, error: true}
-    }
-    errorMessage['cod_paciente'].error = false
-
     this.state = {
       simulation: simulation,
-      errorMessage,
-      formError,
     };
 
     this.handleSimulationSubmit = this.handleSimulationSubmit.bind(this)
     this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleInputBlur = this.handleInputBlur.bind(this)
-    this.onFormValidate = this.onFormValidate.bind(this)
   }
 
   handleInputChange(event) {
@@ -44,40 +33,8 @@ export default class SimulationForm extends React.Component {
     });
   }
 
-  handleInputBlur(event) {
-    let target = event.target,
-      value = target.value,
-      name = target.name,
-      errorMessage = this.state.errorMessage
-
-    if(value === '') {
-      errorMessage[name] = {value: "Campo obrigatório", error: true}
-      this.setState({formError: true})
-    } else {
-      errorMessage[name] = {value: null, error: false}
-    }
-    this.setState({errorMessage})
-  }
-
-  onFormValidate() {
-    let formError = false
-    for(let key in this.state.errorMessage) {
-      if(this.state.errorMessage[key].error === true)
-        formError = true
-    }
-    this.setState({formError: formError})
-    return new Promise((resolve, reject) => {resolve(true)})
-  }
-
   handleSimulationSubmit() {
-    console.log(this.state.simulation)
-    this.onFormValidate().then(() => {
-      if(this.state.formError === false) {
-        this.props.history.push("/simulacao/"+this.state.simulation.cod_paciente, {"simulationData": this.state.simulation})
-      } else {
-        this.props.handleShowMessage("Revise os erros nos campos", messageType.mError)
-      }
-    })
+    this.props.history.push("/simulacao/"+this.state.simulation.cod_paciente, {"simulationData": this.state.simulation})
     event.preventDefault();
   }
 
@@ -90,35 +47,30 @@ export default class SimulationForm extends React.Component {
 
         <TextField
           onChange={this.handleInputChange}
-          onBlur={this.handleInputBlur}
           floatingLabelText="Concentração desejada (ml)"
           name="concentracao_desejada"
           value={this.state.simulation.concentracao_desejada} /><br />
 
         <TextField
           onChange={this.handleInputChange}
-          onBlur={this.handleInputBlur}
           floatingLabelText="Dose (ml)"
           name="dose"
           value={this.state.simulation.dose} /><br />
 
         <TextField
           onChange={this.handleInputChange}
-          onBlur={this.handleInputBlur}
           floatingLabelText="Intervalo (h)"
           name="intervalo"
           value={this.state.simulation.intervalo} /><br />
 
         <TextField
           onChange={this.handleInputChange}
-          onBlur={this.handleInputBlur}
           floatingLabelText="Duração da infusão (h)"
           name="duracao_infusao"
           value={this.state.simulation.duracao_infusao} /><br />
 
         <TextField
           onChange={this.handleInputChange}
-          onBlur={this.handleInputBlur}
           floatingLabelText="Quantidade de doses"
           name="quantidade_doses"
           value={this.state.simulation.quantidade_doses} /><br />
